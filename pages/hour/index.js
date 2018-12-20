39f6f46a5373d3024ad80392605c49ea22d291e5
imwujue <905537641@qq.com>
2018-12-20 14:12:11 +0800

    update1.4.5

--- pages/hour/index.js
+++ pages/hour/index.js
@@ -1,5 +1,7 @@
 // pages/hour/index.js
 var app = getApp();
+var chart = null;
+var wxCharts = require('../../utils/wxcharts.js');
 Page({
 
   /**
@@ -8,7 +10,6 @@ Page({
   data: {
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -65,7 +66,6 @@ Page({
         var city = res.data.result.addressComponent.city;
         var district = res.data.result.addressComponent.district;
         var street = res.data.result.addressComponent.street;
-
         that.setData({
           city: city,
           district: district,
@@ -92,13 +92,66 @@ Page({
       data: parameters,
       success: function (res) {
         var hour = res.data.HeWeather6[0].hourly;
+        var tmp = res.data.HeWeather6[0].now.tmp;
         console.log(hour);
+        console.log(hour[0].time);
+        for(var i=0;i<8;i++){
+          var n = hour[i].time.split(" ");
+          hour[i].time = n[1];
+        }
         that.setData({
-          hour:hour
+          hour:hour,
+          tmp: tmp,
         })
+        that.getChart(hour);
       },
       fail: function (res) { },
       complete: function (res) { },
     })
+  },
+
+  getChart:function(hour){
+    var windowWidth = '', windowHeight = '';    //定义宽高
+    try {
+      var res = wx.getSystemInfoSync();    //试图获取屏幕宽高数据
+      windowWidth = res.windowWidth;   //以设计图750为主进行比例算换
+      windowHeight = res.windowWidth;   //以设计图750为主进行比例算换
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');   //如果获取失败
+    }
+    console.log(hour);
+    chart = new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: [hour[0].time, hour[1].time, hour[2].time, hour[3].time, hour[4].time, hour[5].time, hour[6].time, hour[7].time],
+      series: [{
+        name: '温度',
+        fontColor: "#FFFFFF",
+        data: [hour[0].tmp, hour[1].tmp, hour[2].tmp, hour[3].tmp, hour[4].tmp, hour[5].tmp, hour[6].tmp, hour[7].tmp],
+        format: function (val) {
+          return val + "°";
+        }
+      }],
+      xAxis:{
+        title: "时间",
+        fontColor: "#FFFFFF",
+        titleFontColor: "#FFFFFF",
+      },
+      yAxis: {
+        fontColor: "#FFFFFF",
+        titleFontColor: "#FFFFFF",
+        format: function (val) {
+          return val.toFixed(0);
+        }
+      },
+      width: windowWidth,
+      height: 300,
+      dataLable:false,
+      dataPointShape:true,
+      extra:{
+        legendTextColor: "#FFFFFF"
+      }
+    });
+    
   }
 })
